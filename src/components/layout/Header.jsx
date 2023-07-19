92a970de9384806b5d1bf7951049ebe7dd37a095
Bzhezinskyi <bzhezinskyiillia@gmail.com>
2023-07-19 19:48:19 +0300

    updata

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,16 @@
 import { TbSearch } from 'react-icons/tb';
 
 import Logo from 'components/Logo';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <Logo />
+
+      <Link to="/">Home</Link>
+      <Link to="/mylibrary">My Library</Link>
+
       <TbSearch />
     </header>
   );
